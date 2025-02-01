c0e5ca5e8d2c8274d37603115625f14cd59c2195
Faisal N Ahmed <faisal.ahmed@au.ey.com>
2025-02-01 12:44:12 +1100

    Fix:Blog from cms

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "nhps.com.au", // WordPress Image Domain
+        hostname: "cms.nhps.com.au",
       },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
